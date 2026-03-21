94747c9bbaf11dce0a0a36d478cc10a55f1c8b9f
Harshal Mathur <mathurharshal.in@gmail.com>
2026-03-21 11:24:47 +0530

    Change model file name from final_model.keras to model.h5

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.markdown("---")
 # Load model
 @st.cache_resource
 def load_model():
-    model_path = "final_model.keras"
+    model_path = "model.h5"
     if os.path.exists(model_path):
         return tf.keras.models.load_model(model_path)
     return None
